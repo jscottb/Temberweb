4fc6adc9ef72bda016bb838db266ce236b409499
Scott Beasley <scottbeasley@gmail.com>
2021-10-15 13:15:47 -0400

    Added cookie and header support.

---
+++ Tembertest.tcl
@@ -0,0 +1,36 @@
+#package require Temberweb 1.0
+#package require Tcl      8.5
+source temberweb.tcl 
+
+proc withparms {soc method query_parms url_parms} {
+   set x [dict get $query_parms x]
+   set y [dict get $query_parms y]
+
+   ::Temberweb::response $soc "<html><head>/withparms</head><body><br><br>$x-$y<br><br><a href=\"/\">Home</a><br></body></html>" 200 "html"
+}
+
+proc withurlparms {soc method query_parms url_parms} {
+   ::Temberweb::response $soc "<html><head>/withurlparms</head><body><br><br>$url_parms<br><br><a href=\"/\">Home</a><br></body></html>" 200 "html"
+}
+
+proc post_action {soc method query_parms url_parms} {
+   puts "in post_action"
+
+   set lname [dict get $query_parms lname]
+   set fname [dict get $query_parms fname]
+
+   ::Temberweb::addHeader "Set-Cookie: id=a3fWa7; Expires=Thu, 31 Oct 2027 07:28:00 GMT;"
+   # parms: name value mins_to_live extra_settings
+   ::Temberweb::addCookie "fname" $fname 1
+   ::Temberweb::addCookie "lname" $lname {} "SameSite=Lax; HttpOnly"
+   ::Temberweb::addCookie "name" "$fname$lname" 5
+   ::Temberweb::response $soc "<html><head>/post_action_page</head><body><br><br>Hello $fname $lname<br><br><a href=\"/\">Home</a><br></body></html>" 200 "html"
+}
+
+::Temberweb::addRoute {/withparms} withparms
+::Temberweb::addRoute {/withurlparms} withurlparms
+::Temberweb::addRoute {/post_action_page} post_action "POST"
+
+# Start the server up and set your port, WWW html pages home and images home.
+::Temberweb::run 8050 "./www" "templates" "images"
+ 
---
+++ temberweb.tcl
@@ -0,0 +1,366 @@
+#
+# TemberWeb (TimberWeb) - A simple web handling framework for small systems.
+# Writtin in tcl
+#
+# By Scott Beasley 2016, 2021
+#
+# Based off of Dustmote by Harold Kaplan 1998 - 2002.
+# See: http://wiki.tcl.tk/4333
+#
+
+package provide Temberweb 1.0
+package require Tcl      8.5
+
+# Create the namespace
+namespace eval ::Temberweb {
+   # Export commands
+   namespace export run addRoute contentType http_return response \
+             requestError 404NotFound addCookie addHeader
+
+   variable root "./www"
+   variable pages "templates"
+   variable image_root "images"
+   variable log "log"
+   variable images {.jpg .gif .png .tif .svg}
+   variable default "index.html"
+   variable port 8080
+   variable uri_handlers {}
+   variable logging TRUE
+   variable cookies {}
+   variable headers {}   
+   variable return_codes
+   variable host
+
+   array set return_codes {
+      200 {200 Ok}
+      204 {204 No Content}
+      400 {400 Bad Request}
+      401 {401 Unauthorized}
+      403 {403 Forbidden}
+      404 {404 Not Found}
+      405 {405 Method Not Allowed}
+   }
+
+   variable content_types
+   array set content_types {
+      {}   text/plain
+      txt  text/plain
+      htm  text/html
+      html text/html
+      gif  image/gif
+      png  image/png
+      jpg  image/jpeg
+      json application/json
+      xml  application/xml
+      js   text/javascript
+      css  text/css
+   }
+}
+
+proc ::Temberweb::bgerror {trouble} {puts stdout "bgerror: $trouble"}
+
+proc ::Temberweb::answer {soc host2 port2} {
+   fileevent $soc readable [list ::Temberweb::processRequest $soc]
+}
+
+proc ::Temberweb::processRequest {soc} {
+   variable root
+   variable pages
+   variable default
+   variable images
+   variable image_root
+   variable uri_handlers
+   variable content_types
+   variable return_codes
+   variable logging
+   variable cookies
+   variable headers
+   variable host
+
+   fconfigure $soc -blocking 0
+   set reqLine [gets $soc]
+ 
+   if {[fblocked $soc]} {
+      return
+   }
+
+   if {[string trim $reqLine] eq {}} {
+      close $soc
+      return
+   }
+
+   fileevent $soc readable {}
+   set req_data [split $reqLine "\n ?"]
+
+   if {$logging == TRUE} {
+      log $req_data 
+   }
+
+   # Remove the HTTP/1.1
+   set req_data [lrange $req_data 0 end-1]
+   set method [string trim [lindex $req_data 0]]
+   set path [lindex $req_data 1]
+   set parms [lindex $req_data 2]
+   set content_len 0
+   set post_parms {}
+   set cookies {}
+   set headers {}
+   set host {}
+
+   while 1 {
+      set reqLine [gets $soc]
+
+      if {[string first "CONTENT-LENGTH:" [string toupper [string trim $reqLine]]] != -1} {
+         set content_len [string trim [lindex [string range [string trim $reqLine] 7 end] 1]]
+      }
+
+      if {[string first "COOKIE:" [string toupper [string trim $reqLine]]] != -1} {
+         set cookies [::Temberweb::cookiesTodict [string range $reqLine 8 end]]
+      }
+
+      if {[string first "HOST:" [string toupper [string trim $reqLine]]] == 0} {
+         set host [string trim [string range $reqLine 6 end]]
+      }
+
+      if {[string trim $reqLine] == {}} {
+         # Handle post data
+         if {$method == "POST"} {
+            if {$content_len == 0} {
+               requestError $soc 400
+               return
+            }
+
+            set post_parms [string trim [read $soc $content_len]]
+         }
+
+         break
+      }
+   }
+
+   flush $soc
+
+   set many [string length $path]
+   set last [string index $path [expr {$many-1}]]
+   if {$last eq {/}} {
+      set path $path$default
+   }
+
+   set url_parms [split $path {/}]
+   # See if there is a route handler for this path.
+   foreach handler $uri_handlers {
+      if {[lindex $handler 0] eq "/[lindex $url_parms 1]"} {
+         set url_parms [lrange $url_parms 2 end]
+         # Build a dict of the parms.
+         if {$method == "POST"} {
+            set query_parms_dic [::Temberweb::parmsTodict $post_parms]
+         } else {
+            set query_parms_dic [::Temberweb::parmsTodict $parms]
+         }
+
+         if {[lsearch [lindex $handler 2] $method] >= 0} {
+            eval {[lindex $handler 1] $soc $method $query_parms_dic $url_parms}
+         } else  {
+            requestError $soc 405
+            return
+         }
+
+         return
+      }
+   }
+
+   set ext [file extension $path]
+   if {[string match *$ext* $images]} {
+      set wholeName $image_root/[file tail $path]
+   } else {
+      set wholeName $root/$pages/$path
+   }
+
+   if {[::Temberweb::sendFile $soc $wholeName]} {
+      404NotFound $soc $path
+   }
+}
+
+proc ::Temberweb::done {inChan outChan args} {
+   close $inChan
+   close $outChan
+}
+
+proc ::Temberweb::404NotFound {soc page} {
+   variable return_codes
+
+   puts $soc "HTTP/1.0 $return_codes(404)"
+   puts $soc ""
+   puts $soc "<html><head><title><No such URL.></title></head>"
+   puts $soc "<body><center>"
+   puts $soc "The URL you requested does not exist on this site."
+   puts $soc "</center></body></html>"
+   close $soc
+   return
+}
+
+proc ::Temberweb::requestError {soc error_num} {
+   variable return_codes
+
+   puts $soc "HTTP/1.0 $return_codes($error_num)"
+   puts $soc ""
+   puts $soc "<html><head><title><$return_codes($error_num)></title></head>"
+   puts $soc "<body>$return_codes($error_num)</body></html>"
+   close $soc
+   return
+}
+
+proc ::Temberweb::addRoute {uri callback {method {GET}}} {
+   variable uri_handlers
+
+   lappend uri_handlers [list $uri $callback $method]
+}
+
+proc ::Temberweb::contentType {ext} {
+   variable content_types
+
+   if {![info exists content_types($ext)]} {
+      set ext {}
+   }
+
+   return $content_types($ext)
+}
+
+proc ::Temberweb::http_return {ret_code} {
+   variable return_codes
+
+   if {![info exists return_codes($ret_code)]} {
+      set ret_code {200}
+   }
+
+   return $return_codes($ret_code)
+}
+
+proc ::Temberweb::response {soc data {code "200"} {type ""}} {
+   variable content_types
+   variable headers
+
+   puts $soc "HTTP/1.1 $code"
+   puts $soc "Content-Type: $content_types($type)"
+   foreach header $headers {
+      puts $soc $header 
+      #puts $header
+   }
+   puts $soc "Content-Length: [string length $data]"
+   puts $soc ""
+   puts $soc $data
+   close $soc
+   return
+}
+
+proc ::Temberweb::parmsTodict {parms} {
+   set parms_dict {}
+
+   foreach var [split $parms "&\n"] {
+      set pair [split $var {=}]
+      dict set parms_dict [lindex $pair 0] [lindex $pair 1]
+   }
+
+   return $parms_dict
+}
+
+proc ::Temberweb::cookiesTodict {parms} {
+   set parms_dict {}
+
+   foreach var [split $parms ";\n"] {
+      set pair [split $var {=}]
+      dict set parms_dict [string trim [lindex $pair 0]] [string trim [lindex $pair 1]]
+   }
+
+   return $parms_dict
+}
+
+proc ::Temberweb::addCookie {cookiename cookieval {ttl {}} {attributes {}}} {
+   variable headers
+   variable host
+
+   set line {}
+
+   append line "Set-Cookie: $cookiename=$cookieval;"
+   if {$ttl != {}} {
+      set now [clock seconds]
+      set tzoffset [clock format [clock seconds] -format {%z}]
+      # Revisit the timezone offset maths later. Probably WRONG...
+      append line " Expires=[clock format [clock add $now [expr {$ttl - $tzoffset}] minutes] -format {%a, %d %b %Y %T GMT;}]"
+   }
+
+   if {$attributes != {}} {
+      append line $attributes {;}
+   }  
+
+   lappend headers $line
+   #puts $headers
+}
+
+proc ::Temberweb::addHeader {header} {
+   variable headers
+
+   lappend headers $header
+}
+
+proc ::Temberweb::sendFile {soc file {url_parms ""}} {
+   variable content_types
+   variable return_codes
+
+   if {[catch {set fileChannel [open $file RDONLY]}]} {
+      return 1
+   } else {
+      set ext [file extension $file]
+      fconfigure $fileChannel -translation binary
+      fconfigure $soc -translation binary -buffering full
+      puts $soc "HTTP/1.1 $return_codes(200)"
+      puts $soc "Content-Type: $content_types([string trim $ext {.}])"
+      puts $soc "Connection: close"
+      puts $soc ""
+      fcopy $fileChannel $soc -command [list ::Temberweb::done $fileChannel $soc]
+      return 0
+   }
+}
+
+proc ::Temberweb::log {req_data} {
+   variable log
+   variable fd_log
+
+   set date [clock format \
+             [clock seconds] -format "%a, %d %b %Y %H:%M:%S"]
+   puts $fd_log "$date: $req_data"
+   flush $fd_log
+}
+
+proc ::Temberweb::run {{port_in 8080} {root_in ""} {pages_in ""} {image_root_in ""} {dolog TRUE}} {
+   variable port
+   variable root
+   variable pages
+   variable image_root
+   variable log
+   variable fd_log
+   variable logging
+
+   set port $port_in
+   if {$root_in != {}} {
+      set root [string trimright $root_in {/}]
+   }
+
+   if {$pages_in != {}} {
+      set pages [string trimright $pages {/}]
+   }
+
+   if {$image_root_in != {}} {
+      set image_root [string trimright $image_root_in {/}]
+   }
+
+   if {$dolog == TRUE} {
+      set logging TRUE
+      set fd_log [open $root/$log/access.log "a"]
+      ::Temberweb::log "Server started" 
+   }
+
+   socket -server answer $port
+   vwait forEver
+   close $fd_log
+}
+
